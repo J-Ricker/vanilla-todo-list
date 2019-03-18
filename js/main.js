a66a0b2f3cc91e6c930e642e3052e61bc29966cb
J-Ricker <lvlgrinddesign@gmail.com>
2019-03-18 14:04:56 -0400

    added showfeedback function

--- js/main.js
+++ js/main.js
@@ -18,3 +18,14 @@ itemForm.addEventListener('submit', function (e) {
         showFeedback('Please enter valid value', 'danger');
     }
 });
+
+// show feedback function
+function showFeedback(text, action) {
+
+feedback.classList.add('showItem', `alert-${action}`);
+feedback.innerHTML = `<p>${text}</p>`;
+
+    setTimeout(function () {
+        feedback.classList.remove('showItem', `alert-${action}`);
+    }, 3000);
+}
