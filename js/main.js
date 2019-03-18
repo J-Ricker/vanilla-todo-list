9655511757b16964410c6d2d30ed114cea566f10
J-Ricker <lvlgrinddesign@gmail.com>
2019-03-18 13:57:01 -0400

    grabbed elements

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// get elements
+const itemForm = document.getElementById('itemForm');
+const itemInput = document.getElementById('itemInput');
+const itemList = document.querySelector('.item-list');
+const clearBtn = document.getElementById('clear-list');
+const feedback = document.querySelector('.feedback');
+
+let itemData = [];
