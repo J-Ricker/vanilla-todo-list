64b129ea4df70fc566017cc4c55f6fbc00dd3697
J-Ricker <lvlgrinddesign@gmail.com>
2019-03-18 20:03:34 -0400

    just small change

--- js/main.js
+++ js/main.js
@@ -18,7 +18,8 @@ if(itemData.length > 0) {
         <a href="#" class="complete-item mx-2 item-icon"><i class="far fa-check-circle"></i></a>
         <a href="#" class="edit-item mx-2 item-icon"><i class="far fa-edit"></i></a>
         <a href="#" class="delete-item item-icon"><i class="far fa-times-circle"></i></a>
-        </div>`);
+        </div>`
+        );
         handleItem(singleItem);
     });
 }
